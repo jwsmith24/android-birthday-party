bfd788975485c3c39899338ef3ad11e7265d2513
Jacob Smith <jacob.w.smith97.mil@army.mil>
2026-05-05 10:12:12 -0500

    add invitation screen and card

--- app/src/main/java/com/example/androidbirthdayparty/ui/InvitationScreen.kt
+++ app/src/main/java/com/example/androidbirthdayparty/ui/InvitationScreen.kt
@@ -1,15 +1,18 @@
 package com.example.androidbirthdayparty.ui
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.androidbirthdayparty.data.Invitation
@@ -21,28 +24,23 @@ fun InvitationScreen(invitations: StateFlow<List<Invitation>>) {
 
     val invites = invitations.collectAsState()
 
-    Column (modifier = Modifier.fillMaxSize()) {
+    Column (
+        modifier = Modifier
+        .fillMaxSize()
+            .padding(4.dp)
+            .background(color = MaterialTheme.colorScheme.primaryContainer)
+    ) {
         Row(modifier = Modifier
             .fillMaxWidth()
             .padding(all = 4.dp)
             .border(border = BorderStroke(1.dp, Color.Cyan))
 
         ) {
-            Text(text = "Android Birthday Party")
+            Text(text = "Android Birthday Party", style = MaterialTheme.typography.titleLarge)
         }
         LazyColumn(verticalArrangement = Arrangement.spacedBy(4.dp)) {
             items(invites.value) {invite ->
-                Column (
-                    modifier = Modifier
-                        .border(BorderStroke(1.dp, Color.Black))
-                        .padding(4.dp)
-
-                    ,
-                    verticalArrangement = Arrangement.spacedBy(4.dp)
-                ){
-                    Text(text = invite.name + if (invite.hasPlusOne) " +1" else "")
-                    Text(text = invite.rsvpStatus.displayName())
-                }
+                InvitationCard(invite)
             }
         }
 
